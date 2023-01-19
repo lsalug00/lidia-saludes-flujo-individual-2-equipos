0e53c1435c5a31465c74ed28da232559a0b998bd
lsalug00 <lidiasaludes@gmail.com>
2023-01-19 11:05:54 +0100

    Tercer commit

--- HolaAlumnos.java
+++ HolaAlumnos.java
@@ -2,6 +2,6 @@ public class HolaAlumnos {
 	public static void main(String[] args) {		
 		System.out.println("Hola");
 		System.out.println("Soy Lidia");
-		System.out.println("Y estudio DAM");
+		System.out.println("Y estudio DAM en el IES San Andres");
 	}
 }
