fad00b0013e75faf15d555be94f3ec9042708879
lsalug00 <lidiasaludes@gmail.com>
2023-01-19 10:47:29 +0100

    Segundo commit

--- HolaAlumnos.java
+++ HolaAlumnos.java
@@ -2,5 +2,6 @@ public class HolaAlumnos {
 	public static void main(String[] args) {		
 		System.out.println("Hola");
 		System.out.println("Soy Lidia");
+		System.out.println("Y estudio DAM");
 	}
 }
